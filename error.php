cd63bf79f53f41b404b414a71558788915e632a6
Gabriela Juncos <gabajuncos@gmail.com>
2021-06-30 20:55:49 -0300

    version inicial

---
+++ error.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="estilo.css">
+	<title>ERROR 404!</title>
+</head>
+<body id="body-error">
+	
+    <section class="error-message">
+    	<section class="error-message__container">
+    		<img class="animated bounce" src="imagenes/error.jpg" alt="">
+    		<h4>¡404!</h4>
+    		<p>UPSSS!!! Página no encontrada</p>
+    	</section>
+    </section>
+
+</body>
+</html>
